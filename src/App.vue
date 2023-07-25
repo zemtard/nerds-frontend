593fa3b900099d288d3cd15fe9730ab04f7792d2
zemtard <zemzard@gmail.com>
2023-07-25 09:51:47 +0200

    New project cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,36 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <header>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+<nav>
+  <RouterLink to="/">Home</RouterLink>
+  
+</nav>
+</header>
+<br>
+<h1>Test app</h1>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+<RouterView />
 
-  <RouterView />
 </template>
 
 <style scoped>
+
+
+
+
 header {
   line-height: 1.5;
   max-height: 100vh;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
 nav {
   width: 100%;
-  font-size: 12px;
-  text-align: center;
+  font-size: 30px;
+  text-align: right;
   margin-top: 2rem;
 }
 
@@ -56,30 +52,5 @@ nav a:first-of-type {
   border: 0;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,27 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+
+    <h1>Home Page Component</h1>
+
+  </template>
+  
+  <script>
+  
+  export default {
+  
+  data(){
+    return {
+    }
+  },
+  
+  mounted(){ 
+    
+  },
+  
+  
+  
+  }
+  </script>
+  
+  <style>
+
+  </style>
